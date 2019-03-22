b5027a1efd533f1b36397e2d0821a7b8317604fe
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-03-22 10:47:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * remove Buffer.from() polyfill and drop support for legacy Node.js versions
    * update ESLint from v0.x to v5.x
    * remove JSCS as it's no longer maintained

---
+++ test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {get} = require('https');
+
+const smallestPng = require('.');
+const test = require('tape');
+
+test('smallestPng()', async t => {
+	t.plan(1);
+
+	get('https://raw.githubusercontent.com/mathiasbynens/small/master/png-transparent.png', async response => {
+		const expected = [];
+		let len = 0;
+
+		for await (const chunk of response) {
+			expected.push(chunk);
+			len += chunk.length;
+		}
+
+		t.ok(
+			smallestPng().equals(Buffer.concat(expected, len)),
+			'should create a Buffer of the theoretically smallest PNG.'
+		);
+	});
+});
