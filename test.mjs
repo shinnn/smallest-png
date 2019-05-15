ca97944ef367677110f6086aefe52aba02e00050
Watanabe Shinnosuke <snnskwtnb@gmail.com>
2019-05-15 10:47:57 +0900

    switch from tape to testit
    https://github.com/ForbesLindesay/testit

--- test.mjs
+++ test.mjs
@@ -1,10 +1,11 @@
+import {strict as assert} from 'assert';
 import {get} from 'https';
 import {once} from 'events';
 
 import smallestPng from './index.js';
-import test from 'tape';
+import test from 'testit';
 
-test('smallestPng', async t => {
+test('Be a Buffer of the theoretically smallest PNG', async () => {
 	const [response] = await once(get('https://raw.githubusercontent.com/mathiasbynens/small/master/png-transparent.png'), 'response');
 	const expected = [];
 
@@ -12,10 +13,5 @@ test('smallestPng', async t => {
 		expected.push(chunk);
 	}
 
-	t.ok(
-		smallestPng.equals(Buffer.concat(expected, Number(response.headers['content-length']))),
-		'should be a Buffer of the theoretically smallest PNG.'
-	);
-
-	t.end();
+	assert(smallestPng.equals(Buffer.concat(expected, Number(response.headers['content-length']))));
 });
